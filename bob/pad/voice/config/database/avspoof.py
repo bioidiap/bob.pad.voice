f77688f82380e816098e15bb68e085ef51f1e119
Pavel Korshunov <pavel.korshunov@idiap.ch>
2016-09-21 12:25:40 +0200

    Trigger CI

--- bob/pad/voice/config/database/avspoof.py
+++ bob/pad/voice/config/database/avspoof.py
@@ -4,13 +4,13 @@ import bob.pad.voice.database
 
 
 # directory where the wave files are stored
-# avspoof_input_dir = "/idiap/project/lobi/AVSpoof/data/"  # folder at Idiap
-avspoof_input_dir = "/Users/pavelkor/Documents/pav/idiap/data/AVspoof_full/data/"  # local copy
+avspoof_input_dir = "/idiap/project/lobi/AVSpoof/data/"  # folder at Idiap
+# avspoof_input_dir = "/Users/pavelkor/Documents/pav/idiap/data/AVspoof_full/data/"  # local copy
 avspoof_input_ext = ".wav"
 
 
 database = bob.pad.voice.database.AVspoofPadDatabase(
-    protocol='smalltest',
+    protocol='greattest',
     original_directory=avspoof_input_dir,
     original_extension=avspoof_input_ext,
     training_depends_on_protocol=True,
