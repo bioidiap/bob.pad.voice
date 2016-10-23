7e4e5adf6fe49380863d686611d857df8e7f12bc
Pavel Korshunov <pavel.korshunov@idiap.ch>
2016-10-23 17:45:56 +0200

    Cleaned sphinx exceptions fixed in bob.bio.base

--- bob/pad/voice/database/__init__.py
+++ bob/pad/voice/database/__init__.py
@@ -5,13 +5,26 @@ from .replay import ReplayPadDatabase
 from .replaymobile import ReplayMobilePadDatabase
 from .voicepa import VoicePAPadDatabase
 
-# to fix sphinx warnings of not able to find classes, when path is shortened
-PadVoiceFile.__module__ = "bob.pad.voice.database"
-ASVspoofPadDatabase.__module__ = "bob.pad.voice.database"
-AVspoofPadDatabase.__module__ = "bob.pad.voice.database"
-ReplayPadDatabase.__module__ = "bob.pad.voice.database"
-ReplayMobilePadDatabase.__module__ = "bob.pad.voice.database"
-VoicePAPadDatabase.__module__ = "bob.pad.voice.database"
-
 # gets sphinx autodoc done right - don't remove it
+def __appropriate__(*args):
+  """Says object was actually declared here, and not in the import module.
+  Fixing sphinx warnings of not being able to find classes, when path is shortened.
+  Parameters:
+
+    *args: An iterable of objects to modify
+
+  Resolves `Sphinx referencing issues
+  <https://github.com/sphinx-doc/sphinx/issues/3048>`
+  """
+
+  for obj in args: obj.__module__ = __name__
+
+__appropriate__(
+    PadVoiceFile,
+    ASVspoofPadDatabase,
+    AVspoofPadDatabase,
+    ReplayPadDatabase,
+    ReplayMobilePadDatabase,
+    VoicePAPadDatabase,
+    )
 __all__ = [_ for _ in dir() if not _.startswith('_')]
